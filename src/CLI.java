162ed7597f10a339fae48f30b4c1cefc623f3c4e
Anton <antonstromkarl@gmail.com>
2019-10-09 15:25:33 +0200

    Started test with System.in

--- src/CLI.java
+++ src/CLI.java
@@ -1,10 +1,39 @@
-public class CLI {
+import java.util.Scanner;
 
-    private boolean createPlayers(){
+public class CLI {
+    private String playerOneName, playerTwoName;
+
+    public CLI() {
+    }
+
+    public boolean createPlayers() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter name for player 1");
+        playerOneName = scan.next();
+        while (playerOneName.length() == 0 || playerOneName.length() > 10) {
+            System.out.println("Invalid name. Please enter a new one.");
+            playerOneName = scan.nextLine();
+        }
+        System.out.println("Enter name for player 2");
+        playerTwoName = scan.next();
+        while (playerTwoName.length() == 0 || playerTwoName.length() > 10 || playerOneName.equals(playerTwoName)) {
+            System.out.println("Invalid name. Please enter a new one.");
+            playerTwoName = scan.nextLine();
+        }
+        new Game(playerOneName, playerTwoName);
         return true;
     }
 
-    private boolean gameloop(){
+    private boolean gameloop() {
         return true;
+    }
+
+    public String getPlayerOneName() {
+        return playerOneName;
+    }
+
+    public String getPlayerTwoName() {
+        return playerTwoName;
     }
 }
--- src/test/CLITest.java
+++ src/test/CLITest.java
@@ -1,15 +1,22 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CLITest {
     @Test
     void createPlayers() {
-        String playerOneName = "a";
-        String playerTwoName = "b";
-        assertFalse(playerOneName.isEmpty() && playerTwoName.isEmpty());
-        assertTrue(playerOneName != playerTwoName);
-        assertTrue(playerOneName.length() < 10 && playerTwoName.length() < 10);
+        CLI cli = new CLI();
+        String input = "Anton\nKalle\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertTrue(cli.createPlayers());
+        assertFalse(cli.getPlayerOneName().isEmpty() && cli.getPlayerTwoName().isEmpty());
+        assertTrue(cli.getPlayerOneName() != cli.getPlayerTwoName());
+        assertTrue(cli.getPlayerOneName().length() < 10 && cli.getPlayerTwoName().length() < 10);
     }
 
     @Test
