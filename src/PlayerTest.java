ea136ddc79c77f108d25151b72d798555fd20da1
Peter Sjögren <peter.sjogren@live.se>
2019-10-09 11:13:52 +0200

    playertest

---
+++ src/PlayerTest.java
@@ -0,0 +1,40 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+	@org.junit.jupiter.api.Test
+	void getName() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void getHealth() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void setHealth() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void getMana() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void setMana() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void getCardsOnHand() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void setCardsOnHand() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void getCardsOnTable() {
+	}
+
+	@org.junit.jupiter.api.Test
+	void setCardsOnTable() {
+	}
+}
