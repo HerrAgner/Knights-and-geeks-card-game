18471650a2bec454f7770901ef62b7a8ff5a04d8
Ted <ted.agner@hotmail.com>
2019-10-09 11:27:30 +0200

    Moved test to test-package

---
+++ src/test/PlayerTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import org.junit.jupiter.api.*;
+
+
+class PlayerTest {
+
+	@Test
+	void getName() {
+	}
+
+	@Test
+	void getHealth() {
+	}
+
+	@Test
+	void setHealth() {
+	}
+
+	@Test
+	void getMana() {
+	}
+
+	@Test
+	void setMana() {
+	}
+
+	@Test
+	void getCardsOnHand() {
+	}
+
+	@Test
+	void setCardsOnHand() {
+	}
+
+	@Test
+	void getCardsOnTable() {
+	}
+
+	@Test
+	void setCardsOnTable() {
+	}
+}
