96b97635f7006e5db091e775d28e3136666e592a
Anton <antonstromkarl@gmail.com>
2019-10-09 11:46:11 +0200

    new test file

---
+++ src/test/CLITest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CLITest {
+    @Test
+    void createPlayers(){
+
+    }
+
+    @Test
+    void gameloop(){
+
+    }
+}
