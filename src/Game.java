e705b12109eda24a87a7f314447decf63945174f
Eric Rasmusson Lindström <eric.rl@me.com>
2019-10-09 11:03:19 +0200

    GameClass

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,42 @@
+import cards.Card;
+
 public class Game {
+
+
+    public boolean drawCard() {
+
+        return true;
+    }
+
+    public boolean playCard() {
+
+        return true;
+    }
+
+    public boolean attackCard(Card playedCard, Card enemyCard){
+
+        return true;
+    }
+
+    public boolean attackPlayer(Card card){
+
+        return true;
+    }
+
+    public boolean finishTurn(){
+
+        return true;
+    }
+
+    public boolean finishGame(){
+
+        return true;
+    }
+
+    public boolean initGame(Player p1, Player p2){
+
+        return true;
+    }
+
     
 }
