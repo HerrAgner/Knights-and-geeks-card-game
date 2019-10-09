222b4867842b8da818dc117ff201c9badcd08f97
Ted <ted.agner@hotmail.com>
2019-10-09 11:34:49 +0200

    Added unitcard test

---
+++ src/test/UnitCardTest.java
@@ -0,0 +1,33 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UnitCardTest {
+
+    @Test
+    void superConstructorTest() {
+    }
+
+    @Test
+    void constructorTest() {
+    }
+
+    @Test
+    void getHp() {
+    }
+
+    @Test
+    void getAttack() {
+    }
+
+    @Test
+    void setHp() {
+    }
+
+    @Test
+    void setAttack() {
+    }
+
+}
