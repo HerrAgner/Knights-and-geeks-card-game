7c1fa2a4bd9259951e9db84709a8d3dbe0aa3d66
Anton <antonstromkarl@gmail.com>
2019-10-10 16:14:20 +0200

    added a fun ascii api for the winner text

---
+++ src/HttpGet.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpGet {
+    private String name;
+    private int round;
+
+    public HttpGet(String name, int round) {
+        this.name = name;
+        this.round = round;
+    }
+
+    public void sendGet() throws Exception {
+        String url = "http://artii.herokuapp.com/make?text=" + name + "+won+the+game+in+" + round +"+rounds";
+        URL obj = new URL(url);
+
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        con.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine);
+            response.append(inputLine);
+        }
+        in.close();
+
+    }
+}
