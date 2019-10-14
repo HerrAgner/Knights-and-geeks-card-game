262ea1a889f95caae37656a7f56c72d9d6d5d5ca
Ted <ted.agner@hotmail.com>
2019-10-14 12:39:01 +0200

    Started CLI gameloop

--- src/CLI.java
+++ src/CLI.java
@@ -1,6 +1,11 @@
+import cards.Card;
 import cards.UnitCard;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class CLI {
     private String playerOneName, playerTwoName;
@@ -35,24 +40,59 @@ public class CLI {
     }
 
     private void gameloop() {
-        while(running) {
+        while (running) {
 
             Player activePlayer = game.getPlayers()[game.getActivePlayer()];
             System.out.println(activePlayer.getName() + "'s turn");
-            activePlayer.getCardsOnTable().forEach(card -> System.out.printf("'%15s'", card.getName()));
-            activePlayer.getCardsOnTable().forEach(card -> {
-                UnitCard unitCard = (UnitCard) card;
-                System.out.printf("'%15s'", unitCard.getHp());
-            });
-            activePlayer.getCardsOnTable().forEach(card -> {
-                UnitCard unitCard = (UnitCard) card;
-                System.out.printf("'%15s'", unitCard.getAttack());
-            });
+            List<Card> asd = new ArrayList<>(activePlayer.getCardsOnHand());
+
+            printCards(asd);
+
+//            activePlayer.getCardsOnHand().forEach(card -> System.out.printf("%-30s", card.getName()));
+//            System.out.println();
+//            activePlayer.getCardsOnHand().forEach(card -> {
+//                UnitCard unitCard = (UnitCard) card;
+//                System.out.printf("%-30s", "HP: " + unitCard.getHp());
+//            });
+//            System.out.println();
+//            activePlayer.getCardsOnHand().forEach(card -> {
+//                UnitCard unitCard = (UnitCard) card;
+//                System.out.printf("%-30s", "Atk: " + unitCard.getAttack());
+//            });
+            System.out.println();
             System.out.println("Make a move!");
             String s = scan.next();
         }
 
 
+    }
+
+    private void printCards(List<Card> cards) {
+        StringBuilder output = new StringBuilder();
+
+        cards.forEach(card -> {
+            output.append(String.format("%-30s", card.getName()));
+        });
+        output.append("\n");
+        cards.forEach(card -> {
+            if (card instanceof UnitCard) {
+                UnitCard unitCard = (UnitCard) card;
+                output.append(String.format("%-30s", "Hp: " + unitCard.getHp()));
+            }
+        });
+        output.append("\n");
+        cards.forEach(card -> {
+            if (card instanceof UnitCard) {
+                UnitCard unitCard = (UnitCard) card;
+                output.append(String.format("%-30s", "Atk: " + unitCard.getAttack()));
+            }
+        });
+        output.append("\n");
+        cards.forEach(card -> {
+            output.append(String.format("%-30s", "Cost: " + card.getCost()));
+        });
+
+        System.out.println(output);
     }
 
     public String getPlayerOneName() {
