8c33711b9790731921627711833f69321693b090
Johan Schultze <Johan.schultze@gmail.com>
2019-10-14 11:43:50 +0200

    Test for constructor (effectcard)

--- src/test/EffectCardTest.java
+++ src/test/EffectCardTest.java
@@ -1,12 +1,22 @@
+import cards.EffectCard;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EffectCardTest {
+    private EffectCard card;
+
+    @BeforeEach
+    void init(){
+    card = new EffectCard("Anton", 2, "debuff", 0, 3);
+    }
 
     @Test
     void constructorTest(){
-
+    assertNotNull(card);
+    assertEquals(card.getAttack(), 3);
     }
 
     @Test
