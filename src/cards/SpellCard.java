40d393c5c528f9def6de0a51b87a4bf947658123
Eric Rasmusson Lindström <eric.rl@me.com>
2019-10-14 12:45:20 +0200

    More tests spell

--- src/cards/SpellCard.java
+++ src/cards/SpellCard.java
@@ -3,36 +3,55 @@ package cards;
 public class SpellCard extends Card {
     private String type;
     private boolean many;
-    private int hp;
     private int dmg;
+    private int heal;
 
-    public SpellCard(String type, boolean many, int dmg, String name, int cost){
+    public SpellCard(){
+        this("", false, 0, 0, "", 0);
+    }
+
+    public SpellCard(String type, boolean many, int dmg, int heal, String name, int cost){
         super(name, cost);
         this.type = type;
         this.many = many;
-        this.dmg = dmg;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public void setMany(boolean many) {
-        this.many = many;
-    }
-
-    public void setDmg(int dmg) {
-        this.dmg = dmg;
+        if(dmg > 0){
+            this.dmg = dmg;
+            this.heal = 0;
+        } else if (heal > 0) {
+            this.dmg = 0;
+            this.heal = heal;
+        }
     }
 
     public String getType() {
         return type;
     }
 
+    public void setType(String type) {
+        this.type = type;
+    }
+
     public boolean isMany() {
         return many;
     }
+
+    public void setMany(boolean many) {
+        this.many = many;
+    }
+
     public int getDmg() {
         return dmg;
+    }
+
+    public void setDmg(int dmg) {
+        this.dmg = dmg;
+    }
+
+    public int getHeal() {
+        return heal;
+    }
+
+    public void setHeal(int heal) {
+        this.heal = heal;
     }
 }
--- src/test/SpellCardTest.java
+++ src/test/SpellCardTest.java
@@ -10,13 +10,27 @@ class SpellCardTest {
 
     @BeforeEach
     void innit() {
-        healer = new SpellCard("Heal", false, 0, "Eric", 2);
-        attacker = new SpellCard("Attack", false, 5, "Ted", 2);
+        healer = new SpellCard("Heal", false, 0, 2, "Eric", 2);
+        attacker = new SpellCard("Attack", false, 5, 0, "Ted", 2);
     }
 
     @Test
     void constructorTest(){
         assertNotNull(healer);
         assertNotNull(attacker);
+
+        assertEquals(healer.getDmg(), 0);
+        assertEquals(attacker.getHeal(), 0);
+
     }
+
+    @Test
+    void superConstructorTest() {
+        assertNull(new SpellCard().getName());
+        assertEquals(new SpellCard().getCost(), 0);
+        assertNotNull(healer.getId());
+        assertNotEquals(healer.getId(), attacker.getId());
+
+    }
+    
 }
