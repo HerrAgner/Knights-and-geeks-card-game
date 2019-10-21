f243e34c1f2896b53c99c9dab0672a0a335e1126
Johan Schultze <Johan.schultze@gmail.com>
2019-10-21 15:37:38 +0200

    Added tests for validateEmptyTable

--- src/test/InputTest.java
+++ src/test/InputTest.java
@@ -8,8 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InputTest {
 
-        private Input input;
-        private Game game;
+    private Input input;
+    private Game game;
 
     @Test
     void inputValidation() {
@@ -17,8 +17,22 @@ class InputTest {
         input = new Input();
 
         assertFalse(input.inputValidation(5, 6));
-        assertTrue(input.inputValidation(7,3));
+        assertTrue(input.inputValidation(7, 3));
 
     }
 
+    @Test
+    void validateEmptyTable() {
+        input = new Input();
+        game = new Game("Arne", "Kjell", 100);
+        EffectCard card = new EffectCard("Buff", 1, "Hp", 2);
+        EffectCard card1 = new EffectCard("Debuff", 1, "Hp", -2);
+
+        game.getCurrentPlayer().addCardToHand(card);
+        game.getDefendingPlayer().addCardToHand(card1);
+
+
+        assertFalse(input.validateEmptyTable(card.getId(), game.getCurrentPlayer(), game.getDefendingPlayer()));
+        assertTrue(input.validateEmptyTable(card1.getId(), game.getCurrentPlayer(), game.getDefendingPlayer()));
+    }
 }
